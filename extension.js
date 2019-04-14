73f34ecea924954f34f14415eaeeb11ec7b468b7
Felicio Mununga <felicio@users.noreply.github.com>
2019-04-14 21:13:51 +0200

    Extract command execution to a function
    - Defines general eventListener
    - Defines startedDebugSessionListener and terminatedDebugSessionListener

--- extension.js
+++ extension.js
@@ -1,91 +1,107 @@
 const vscode = require('vscode')
 
 function activate(context) {
-  const disposable = vscode.debug.onDidStartDebugSession(() => {
-    // Get event configuration
-    const configurations = vscode.workspace
-      .getConfiguration('events')
-      .get('configurations')
-    const eventConfigurations = configurations.filter(
-      configuration => configuration.event === 'onDidStartDebugSession',
-    )
-    const configuration = eventConfigurations[eventConfigurations.length - 1]
+  const startedDebugSessionListener = vscode.debug.onDidStartDebugSession(() =>
+    eventListener('onDidStartDebugSession'),
+  )
+  const terminatedDebugSessionListener = vscode.debug.onDidTerminateDebugSession(
+    () => eventListener('onDidTerminateDebugSession'),
+  )
 
-    // Reduce commands to unique commands
-    let reducedCommands = configuration.commands.reduce(
-      (accumulatedCommands, currentCommand) => {
-        const index = accumulatedCommands.findIndex(
-          command => command.command === currentCommand.command,
-        )
+  context.subscriptions.push(
+    startedDebugSessionListener,
+    terminatedDebugSessionListener,
+  )
+}
 
-        if (index === -1) {
-          accumulatedCommands.push(currentCommand)
-        } else {
-          accumulatedCommands[index] = currentCommand
-        }
+function eventListener(event) {
+  // Get event configuration
+  const configurations = vscode.workspace
+    .getConfiguration('events')
+    .get('configurations')
+  const eventConfigurations = configurations.filter(
+    configuration => configuration.event === event,
+  )
+  const configuration = eventConfigurations[eventConfigurations.length - 1]
 
-        return accumulatedCommands
-      },
-      [],
-    )
-
-    // Reduce commands' conditions to unique conditions
-    reducedCommands = reducedCommands.map(reducedCommand => {
-      const command = reducedCommand.conditions.reduce(
-        (command, currentCondition) => {
-          const index = command.conditions.findIndex(
-            condition =>
-              condition.configuration === currentCondition.configuration,
-          )
-
-          if (index === -1) {
-            command.conditions.push(currentCondition)
-          } else {
-            command.conditions[index] = currentCondition
-          }
-
-          return command
-        },
-        { command: reducedCommand.command, conditions: [] },
+  // Reduce commands to unique commands
+  let reducedCommands = configuration.commands.reduce(
+    (commands, currentCommand) => {
+      const index = commands.findIndex(
+        command => command.command === currentCommand.command,
       )
 
-      return command
-    })
-
-    // Reduce commands to commands with valid conditions
-    reducedCommands = reducedCommands.reduce(
-      (accumulatedCommands, currentCommand) => {
-        const isValid = currentCommand.conditions.every(condition => {
-          const workspaceConfiguration = vscode.workspace
-            .getConfiguration()
-            .get(condition.configuration)
-
-          if (workspaceConfiguration === undefined) {
-            console.error('Invalid configuration')
-          }
-
-          // FIXME: Add object type comparison as well
-          return workspaceConfiguration === condition.value
+      if (index >= 0) {
+        commands[index] = {
+          ...currentCommand,
+          conditions: currentCommand.conditions.map(condition => ({
+            ...condition,
+          })),
+        }
+      } else {
+        commands.push({
+          ...currentCommand,
+          conditions: currentCommand.conditions.map(condition => ({
+            ...condition,
+          })),
         })
+      }
 
-        if (isValid) {
-          accumulatedCommands.push(currentCommand.command)
+      return commands
+    },
+    [],
+  )
+
+  // Reduce commands' conditions to unique conditions
+  reducedCommands = reducedCommands.map(reducedCommand => {
+    const command = reducedCommand.conditions.reduce(
+      (command, currentCondition) => {
+        const index = command.conditions.findIndex(
+          condition =>
+            condition.configuration === currentCondition.configuration,
+        )
+
+        if (index >= 0) {
+          command.conditions[index] = { ...currentCondition }
+        } else {
+          command.conditions.push({ ...currentCondition })
         }
 
-        return accumulatedCommands
+        return command
       },
-      [],
+      { ...reducedCommand, conditions: [] },
     )
 
-    // Execute each command
-    if (reducedCommands.length) {
-      reducedCommands.forEach(command => {
-        vscode.commands.executeCommand(command)
-      })
-    }
+    return command
   })
 
-  context.subscriptions.push(disposable)
+  // Reduce commands to commands with valid conditions
+  reducedCommands = reducedCommands.reduce((commands, currentCommand) => {
+    const isValid = currentCommand.conditions.every(condition => {
+      const configuration = vscode.workspace
+        .getConfiguration()
+        .get(condition.configuration)
+
+      if (configuration === undefined) {
+        console.error('Invalid configuration')
+      }
+
+      return configuration === condition.value
+    })
+
+    if (isValid) {
+      commands.push(currentCommand.command)
+    }
+
+    return commands
+  }, [])
+
+  // Execute each command
+  if (reducedCommands.length) {
+    reducedCommands.forEach(command => {
+      vscode.commands.executeCommand(command)
+    })
+  }
 }
 
 exports.activate = activate
